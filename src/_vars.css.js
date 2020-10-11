2ee555221b2711f12d4969b99ff276573f48af5c
Adriel Cavalcanti <cavalcantiadriel@gmail.com>
2020-10-10 22:58:29 -0300

    config eslint, prettier and husky

--- src/_vars.css.js
+++ src/_vars.css.js
@@ -1,7 +1,8 @@
 export const font = 'sans-serif';
-export const colors = { 
-    black : '#2b2929', 
-    red : 'crimson',
-    white : '#fff',
-    blue : '#0090ff'
-    };
+
+export const colors = {
+  black: '#2b2929',
+  red: 'crimson',
+  white: '#fff',
+  blue: '#0090ff',
+};
