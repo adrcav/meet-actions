0b66a89786582f29a2e51c618fcc54b9403ffb81
Adriel Cavalcanti <cavalcantiadriel@gmail.com>
2020-10-17 12:23:51 -0300

    feat: adding default emojis structure

--- src/containers/Navbar/index.js
+++ src/containers/Navbar/index.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import './style.scss'
+import React from 'react';
+import Twemoji from 'react-twemoji';
+import './style.scss';
+
+import { emojis } from '../../data/emojis';
 
 const Navbar = () => {
+  return (
+    <div className="ma-navbar">
+      {emojis.map((emoji) => (
+        <Twemoji key={emoji.name} options={{ className: 'ma-navbar__emoji' }}>
+          <span role="img" aria-label={emoji.name} title={emoji.description}>
+            {emoji.value}
+          </span>
+        </Twemoji>
+      ))}
 
-  return(
-    <div className="ma-navbar" />
-  )
+      <Twemoji key="configuration" options={{ className: 'ma-navbar__emoji' }}>
+        <span role="img" aria-label="Configuration" title="Settings">
+          ⚙️
+        </span>
+      </Twemoji>
+    </div>
+  );
+};
 
-}
-
-export default Navbar
+export default Navbar;
---
+++ src/data/emojis.js
@@ -0,0 +1,12 @@
+export const emojis = [
+  {
+    name: 'Raised',
+    value: '✋',
+    description: 'Wave your hand',
+  },
+  {
+    name: 'Claps',
+    value: '👏',
+    description: 'Claps your hands',
+  },
+];
